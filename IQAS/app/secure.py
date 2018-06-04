8f607628be39ff764ecca8cafaf4e2940659831d
vhbn <1016372899@qq.com>
2018-06-04 14:18:59 +0800

    delete all

--- IQAS/app/secure.py
+++
@@ -1,4 +0,0 @@
-
-DEBUG = True
-SECRET_KEY = "dsadsdajdklajskl123sadsa"
-SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:@localhost:3306/iqsa_es'
--- IQAS/app/web/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-web = Blueprint('web', __name__)
-
-from app.web import auth
-from app.web import main
-from app.web import questions_answers
